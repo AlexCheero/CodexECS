8a89df731ec17256ddf39164838e9e8a3ae152d2
alexcheero <alexcheero157@gmail.com>
2022-05-04 00:15:09 +0300

    filter serialization

--- EcsFilter.cs
+++ EcsFilter.cs
@@ -157,17 +157,70 @@ namespace ECS
 
         public int ByteLength()
         {
-            throw new NotImplementedException();
+            int length = Includes.ByteLength;
+            length += Excludes.ByteLength;
+            length += sizeof(int); //_filteredEntities.Count
+            length += 2 * sizeof(int) * _filteredEntities.Count;
+            length += 2 * sizeof(int);//_entitiesVector.Length and _entitiesVector._elements.Length
+            length += sizeof(int) * _entitiesVector.Length;
+            length += sizeof(int); //_addSet.Count
+            length += sizeof(int) * _addSet.Count;
+            length += sizeof(int); //_removeSet.Count
+            length += sizeof(int) * _removeSet.Count;
+
+            return length;
         }
 
         public void Serialize(byte[] outBytes, ref int startIndex)
         {
-            throw new NotImplementedException();
+            Includes.Serialize(outBytes, ref startIndex);
+            Excludes.Serialize(outBytes, ref startIndex);
+            
+            BinarySerializer.SerializeInt(_filteredEntities.Count, outBytes, ref startIndex);
+            foreach (var pair in _filteredEntities)
+            {
+                BinarySerializer.SerializeInt(pair.Key, outBytes, ref startIndex);
+                BinarySerializer.SerializeInt(pair.Value, outBytes, ref startIndex);
+            }
+            
+            BinarySerializer.SerializeInt(_entitiesVector.Length, outBytes, ref startIndex);
+            BinarySerializer.SerializeInt(_entitiesVector._elements.Length, outBytes, ref startIndex);
+            for (int i = 0; i < _entitiesVector.Length; i++)
+                BinarySerializer.SerializeInt(_entitiesVector[i], outBytes, ref startIndex);
+
+            BinarySerializer.SerializeInt(_addSet.Count, outBytes, ref startIndex);
+            foreach (int idx in _addSet)
+                BinarySerializer.SerializeInt(idx, outBytes, ref startIndex);
+            BinarySerializer.SerializeInt(_removeSet.Count, outBytes, ref startIndex);
+            foreach (int idx in _removeSet)
+                BinarySerializer.SerializeInt(idx, outBytes, ref startIndex);
         }
 
         public void Deserialize(byte[] bytes, ref int startIndex)
         {
-            throw new NotImplementedException();
+            Includes.Deserialize(bytes, ref startIndex);
+            Excludes.Deserialize(bytes, ref startIndex);
+
+            int filteredEntitiesCount = BinarySerializer.DeserializeInt(bytes, ref startIndex);
+            for (int i = 0; i < filteredEntitiesCount; i++)
+            {
+                int key = BinarySerializer.DeserializeInt(bytes, ref startIndex);
+                int value = BinarySerializer.DeserializeInt(bytes, ref startIndex);
+                _filteredEntities.Add(key, value);
+            }
+
+            _entitiesVector._end = BinarySerializer.DeserializeInt(bytes, ref startIndex);
+            var entitesElementsLength = BinarySerializer.DeserializeInt(bytes, ref startIndex);
+            _entitiesVector._elements = new int[entitesElementsLength];
+            for (int i = 0; i < _entitiesVector.Length; i++)
+                _entitiesVector[i] = BinarySerializer.DeserializeInt(bytes, ref startIndex);
+
+            int addSetCount = BinarySerializer.DeserializeInt(bytes, ref startIndex);
+            for (int i = 0; i < addSetCount; i++)
+                _addSet.Add(BinarySerializer.DeserializeInt(bytes, ref startIndex));
+            int removeSetCount = BinarySerializer.DeserializeInt(bytes, ref startIndex);
+            for (int i = 0; i < removeSetCount; i++)
+                _removeSet.Add(BinarySerializer.DeserializeInt(bytes, ref startIndex));
         }
     }
 }
