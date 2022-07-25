3e956e66c7e86b4e9eaeb8307a0302c28909a4cd
alexcheero <alexcheero157@gmail.com>
2022-07-25 23:22:29 +0300

    Getting component's id as static method

--- EcsSystem.cs
+++ EcsSystem.cs
@@ -4,7 +4,7 @@ namespace ECS
     //TODO: add check that Includes and Excludes doesn't intersects
     public abstract class EcsSystem
     {
-        protected int Id<T>() => ComponentMeta<T>.Id;
+        protected static int Id<T>() => ComponentMeta<T>.Id;
 
         public abstract void Tick(EcsWorld world);
     }
