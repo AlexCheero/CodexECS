27fe33b88718e32e15c183dc2abc1f90d4666b82
Alexander Cherkasov <alexcheero157@gmail.com>
2024-09-24 16:26:48 +0200

    PoolFactory.FactoryMethods stored in sparse set

--- ComponentMeta.cs
+++ ComponentMeta.cs
@@ -52,8 +52,8 @@ namespace CodexECS
 
     internal static class PoolFactory
     {
-        internal static readonly Dictionary<int, Func<int, IComponentsPool>> FactoryMethods;
+        internal static readonly SparseSet<Func<int, IComponentsPool>> FactoryMethods;
 
-        static PoolFactory() => FactoryMethods = new Dictionary<int, Func<int, IComponentsPool>>();
+        static PoolFactory() => FactoryMethods = new ();
     }
 }
