641c58097e5cf203b9c6225a50fc432d82f5333e
Alexander Cherkasov <alexcheero157@gmail.com>
2025-05-01 18:36:37 +0200

    system IsEnabled and IsPausable removed

--- EcsSystem.cs
+++ EcsSystem.cs
@@ -1,11 +1,12 @@
-﻿namespace CodexECS
+﻿
+namespace CodexECS
 {
     //TODO: add check that Includes and Excludes doesn't intersects
     public abstract class EcsSystem
     {
         protected static int Id<T>() => ComponentMeta<T>.Id;
 
-        public virtual bool IsPausable => true;
+        public abstract bool IsEnabled { get; }
 
         public virtual void Init(EcsWorld world) { }
         public virtual void Tick(EcsWorld world) { }
