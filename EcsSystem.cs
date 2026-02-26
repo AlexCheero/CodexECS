c9426ba503f42de7e330182895a5a1a0332567f2
Alexander Cherkasov <alexcheero157@gmail.com>
2026-02-26 20:15:58 +0100

    EcsSystem.CreationPredicateName added

--- EcsSystem.cs
+++ EcsSystem.cs
@@ -4,6 +4,8 @@ namespace CodexECS
     //TODO: add check that Includes and Excludes doesn't intersects
     public abstract class EcsSystem
     {
+        public const string CreationPredicateName = "CreationPredicate";
+        
         protected static int Id<T>() => ComponentMeta<T>.Id;
 
         public virtual void Init(EcsWorld world) { }
